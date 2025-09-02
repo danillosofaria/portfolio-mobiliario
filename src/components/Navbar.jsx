e31a104380e6f91a48af4cd53ec88a3e3cef0a9f
danillosofaria <danillosofaria@gmail.com>
2025-09-02 22:30:55 +0100

    cria componente Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Navbar() {
+    return (
+        <nav>
+            <img src="" alt="" />
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">Sobre</a></li>
+                <li><a href="/products">Produtos</a></li>
+                <li><a href="/contact">Contato</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
