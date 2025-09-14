e8043a227ff4af69fe22d4a0aa3f14510dd4a46e
danillosofaria <danillosofaria@gmail.com>
2025-09-14 17:30:56 +0100

    aleraçao do componente para receber conteudo dinamico

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,15 +1,15 @@
 import './CardProduct.css'
 
-function CardProduct (){
-    return (
-        <div className="CardProduct">
-            <img src="https://placehold.co/400" alt="" />
-            <div className="CardProduct-description">
-                <h4 className="CardProduct-name">Nome do produto</h4>
-                <p className="CardProduct-category">categoria</p>
-            </div>
-        </div>
-    )
+function CardProduct({ image, name, category }) {
+  return (
+    <div className="CardProduct">
+      <img src={image} alt={name} />
+      <div className="CardProduct-description">
+        <h4 className="CardProduct-name">{name}</h4>
+        <p className="CardProduct-category">{category}</p>
+      </div>
+    </div>
+  );
 }
 
 export default CardProduct
