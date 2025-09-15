fff2bf3945dc73e01e99e0933fb7f5257e30583b
danillosofaria <danillosofaria@gmail.com>
2025-09-15 21:11:07 +0100

    Criaçao do products-section e passei o product-list para este componente

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import NewsSection from "../components/NewsSection";
 import CardProduct from "../components/CardProduct";
 import CardList from "../components/CardList";
+import ProductsSection from "../components/ProductsSection";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
 
       <main className="main-home">
         <NewsSection></NewsSection>
-        <CardList></CardList>
+        <ProductsSection></ProductsSection>
       </main>
     </>
   );
