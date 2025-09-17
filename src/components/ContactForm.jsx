5f812f16346224a001dd853dd50b2b31b7a3c8c7
danillosofaria <danillosofaria@gmail.com>
2025-09-17 23:46:26 +0100

    pagina contact finalizada

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./ContactForm.css";
+import Button_more from "./Button_more";
 
 function ContactForm() {
   const [formData, setFormData] = useState({
@@ -22,12 +23,15 @@ function ContactForm() {
   return (
     <section className="contact-section">
       <h2>Contact</h2>
-      <p>Make contact with us_________:</p>
+      <div id="container-text-contact">
+        <span>Rua Tardariz, 323. 4510-442, Porto - Portugal</span>
+        <span>Tel: +351 938545781</span>
+      </div>
       <form className="contact-form" onSubmit={handleSubmit}>
         <input
           type="text"
           name="name"
-          placeholder="Seu nome"
+          placeholder="Name"
           value={formData.name}
           onChange={handleChange}
           required
@@ -35,19 +39,21 @@ function ContactForm() {
         <input
           type="email"
           name="email"
-          placeholder="Seu e-mail"
+          placeholder="E-mail"
           value={formData.email}
           onChange={handleChange}
           required
         />
         <textarea
           name="message"
-          placeholder="Sua mensagem"
+          placeholder="Your mensage"
           value={formData.message}
           onChange={handleChange}
           required
         />
-        <button type="submit">Enviar</button>
+        <div id="button-position">
+        <button type="submit">Send</button>
+        </div>
       </form>
     </section>
   );
